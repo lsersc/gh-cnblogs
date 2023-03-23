056effbbd67cd17a6646c523627517927590ed60
lsersc <126891201+lsersc@users.noreply.github.com>
2023-03-24 07:52:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="github.com";  //你需要反代的域名
+        url.hostname="fancy-bonus-e739.giwixi7295.workers.dev";  //你需要反代的域名
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
