b8e3390ad99598b33219063a6df1abfdefa9269a
lsersc <126891201+lsersc@users.noreply.github.com>
2023-03-28 12:50:43 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
+      if (url.pathname.startsWith('/nofta')) {
         url.hostname="www.cnblogs.com/nofta";  //你需要反代的域名
         let new_request=new Request(url,request);
         return fetch(new_request);
